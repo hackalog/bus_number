80491cf7f65dc1345c549aaa3028cc17963ac8d5
Amy Wooding <amy@wooding.org>
2018-10-11 13:43:32 -0400

    add predict functionality

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,35 +1,38 @@
 # -*- coding: utf-8 -*-
-import click
-import json
-import joblib
 import os
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-from functools import partial
-
-import numpy as np
-import pandas as pd
+from ..utils import save_json, load_json
+import click
 
 from ..logging import logger
 from ..paths import model_path, model_output_path
-from ..data import datasets
 from . import run_model
 
 @click.command()
 @click.argument('model_list')
 @click.option('--output_file', '-o', nargs=1, type=str, default='predictions.json')
 @click.option('--hash-type', '-H', type=click.Choice(['md5', 'sha1']), default='sha1')
-def main(model_list, output_file=None, hash_type='sha1'):
+def main(model_list, *, output_file, hash_type):
     logger.info(f'Executing models from {model_list}')
 
     os.makedirs(model_output_path, exist_ok=True)
 
-    with open(model_path / model_list) as f:
-        predict_list = json.load(f)
+    predict_list = load_json(model_path / model_list)
 
+    saved_meta = {}
+    metadata_keys = ['dataset_name', 'hash_type', 'data_hash', 'target_hash', 'experiment']
     for exp in predict_list:
-        run_model(**exp)
+        ds = run_model(**exp)
+        name = ds.metadata['dataset_name']
+        metadata = {}
+        for key in metadata_keys:
+            metadata[key] = ds.metadata[key]
+            saved_meta[name] = metadata
 
+    save_json(model_path / output_file, saved_meta)
+    logger.info(f"Predict complete! Results in {model_path / output_file}")
+    
 if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
