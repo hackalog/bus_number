35a644060be4ee378835e606aa38d81d1197b56f
Amy Wooding <amy@wooding.org>
2018-10-14 14:32:49 -0400

    add required paths

--- src/paths.py
+++ src/paths.py
@@ -4,7 +4,7 @@ import pathlib
 # Get the project directory as the parent of this module location
 src_module_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 project_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent
-                          
+
 data_path = project_dir / 'data'
 
 raw_data_path = data_path / 'raw'
@@ -15,4 +15,7 @@ model_path = project_dir / 'models'
 
 trained_model_path = model_path / 'trained'
 model_output_path = model_path / 'predictions'
-analysis_path = model_path / 'analysis'
+
+reports_path = project_dir / 'reports'
+
+summary_path = reports_path / 'summaries'
